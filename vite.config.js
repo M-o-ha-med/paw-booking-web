6c49f4cd030ce58ceca1245c3a8bae85652a937c
amalsantosa <120997152+IchlashulAmalSantosa@users.noreply.github.com>
2024-12-25 17:05:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "BOBO",
+        short_name: "BOBO-Booking",
+        description: "BOBO booking hotel kini hadir untuk Anda.",
+        theme_color: "#007bff",
+        background_color: "#f0f8ff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/assets/bobo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/bobo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 5173,
   },
-})
+});
